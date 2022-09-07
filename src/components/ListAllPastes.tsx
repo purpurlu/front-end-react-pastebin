b6e777beae0f8680d557f198cc2430eb08052ed4
purpurlu <laurazurbayeva@gmail.com>
2022-09-07 16:39:16 +0200

    refresh page afte adding new paste

--- src/components/ListAllPastes.tsx
+++ src/components/ListAllPastes.tsx
@@ -10,13 +10,15 @@ interface Paste {
   body: string;
 }
 
-function ListAllPastes(): JSX.Element {
+interface RefreshProps {
+    refresh: boolean
+}
+
+function ListAllPastes({refresh} : RefreshProps): JSX.Element {
   const [pastes, setPastes] = useState<Paste[]>([]);
 
   useEffect(() => {
-    const api_url = process.env.REACT_APP_LOCAL
-      ? process.env.REACT_APP_API_LOCAL
-      : process.env.REACT_APP_API_HEROKU;
+    const api_url = process.env.REACT_APP_API_BASE;
     const getAllPastes = async () => {
       console.log(api_url);
       const response = await axios.get(`${api_url}pastes/`);
@@ -26,28 +28,10 @@ function ListAllPastes(): JSX.Element {
       }
     };
     getAllPastes();
-  }, []);
+  }, [refresh]);
 
   return (
     <div className="text-center">
-      {/* <table>
-                <thead> 
-                <tr>
-                    <th>Title</th>
-                    <th>Body</th>
-                </tr>
-                </thead>
-                <tbody> 
-                {pastes.map((paste, index) => {
-                    return (
-                        <tr key={index}> 
-                            <td>{paste.title}</td>
-                            <td>{paste.body}</td>
-                        </tr>
-                    )
-                })}
-                </tbody>
-            </table> */}
       <Row>
         {pastes.map((paste, index) => {
           return (
